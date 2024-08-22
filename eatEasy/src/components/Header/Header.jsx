3cbf5ab6e1c3e6dea2d75775da020dde41843326
Ellie Parry <ellierparry@gmail.com>
2024-08-22 22:59:43 +0100

    Updated boilerplate files, browser not showing any content.

---
+++ eatEasy/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from  "./header.module.css";
+
+export default function Header () {
+    return (   
+        <>     
+            <header className={styles.headerContainer}>
+              <h1>eatEasy</h1>
+            </header>
+        </> 
+    );
+}
+
---
+++ eatEasy/src/home/home.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import "./page.css";
+
+export default function HomePage() {
+  return (
+    <>
+    <main className="main">
+        <h1>HELLO PLEASE SHOW</h1>
+    </main>
+    </>
+  );
+}
